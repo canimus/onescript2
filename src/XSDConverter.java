fab4bb450630d684e6eaa71ef85345eb8f5350c9
Herminio Vazquez <canimus@gmail.com>
2017-01-30 08:26:10 +0100

    Addition of H2 database library and examples

---
+++ src/XSDConverter.java
@@ -0,0 +1,16 @@
+import jdk.internal.org.xml.sax.InputSource;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+import java.io.StringReader;
+
+/**
+ * Created by hvazquez on 1/29/2017.
+ */
+public class XSDConverter {
+  public static void main(String[] args) throws Exception {
+    String xml = "<resp><status>good</status><msg>hi</msg></resp>";
+
+  }
+
+}
